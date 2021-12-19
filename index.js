2e76a59bad97b5a0dad363d99b53c9ee04a34962
jihadChowdhory <mohammadjihad0333@gmail.com>
2021-12-19 08:03:24 +0600

    modified color code

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 9999;
 
 app.get("/", (req, res) => {
   res.send(
-    "<h1 style='color:#32cd32;'>Scribe server is running...</h1><p>Powered by - Jihad Chowdhory</p>"
+    "<h1 style='color:#15A14F;'>Scribe server is running...</h1><p>Powered by - Jihad Chowdhory</p>"
   );
 });
 
